ad4f2a43d0eeb5d5fcb37c28659d49c773a5d9ce
Jennifer Holliday <jennifer.holliday@gmail.com>
2021-08-23 16:38:30 -0600

    Set up Function for City Search

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
-//form = city-search-bar
-//button = city-search-button
+//form = city-form
+//input = city-search-bar
+//city= city-name
+
+function searchCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#city-search-bar");
+
+  let namePlacement = document.querySelector("#city-name");
+  namePlacement.innerHTML = `${searchInput.value}`;
+
+  let citySearch = document.querySelector("#city-form");
+  citySearch.addEventListener("submit", searchCity);
+}
