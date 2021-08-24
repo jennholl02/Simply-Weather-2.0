b9c0ea41e721315363b57605c7d7a5ab9018f111
Jennifer Holliday <jennifer.holliday@gmail.com>
2021-08-24 13:01:13 -0600

    Created JS for Date and Time

--- src/index.js
+++ src/index.js
@@ -12,4 +12,44 @@ function cityInput(event) {
 let searchBar = document.querySelector("#city-form");
 searchBar.addEventListener("submit", cityInput);
 
-console.log(cityInput);
+let date = new Date();
+let today = date.getDate();
+
+let hour = date.getHours();
+let ampm = hour >= 12 ? "pm" : "am";
+hour = hour % 12;
+hour = hour ? hour : 12;
+
+let minutes = date.getMinutes();
+minutes = minutes < 10 ? "0" + minutes : minutes;
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let day = days[date.getDay()];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[date.getMonth()];
+
+let currentDateTime = document.querySelector("#time-date-updated");
+currentDateTime.innerHTML = `${day}, ${month} ${today} at ${hour}:${minutes} ${ampm}`;
